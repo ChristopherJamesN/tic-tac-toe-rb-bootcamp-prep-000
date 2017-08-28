4c49b190c0d6ed8ec054d1d301caf9f4d5f4c371
Chris Nady <nady.christopher@gmail.com>
2017-08-28 22:27:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,13 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+    end
+  end
+  counter
+end
